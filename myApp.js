6844574bd48cfa715e4d0d08a1fb905f1c377d3e
sergioadair <sergioadairlopez@gmail.com>
2024-09-30 20:19:19 -0700

    Created a model

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
+mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-let Person;
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
